cdcfee785b907b580f26ff864d028c42e790c2ae
Robert Davis <robdave22@yahoo.com>
2021-12-21 14:45:49 -0500

    Calculator complete

---
+++ calc/Arithmetic.java
@@ -0,0 +1,37 @@
+package calc;
+
+import java.lang.Math;
+
+public class Arithmetic {
+	public static float add(float num1, float num2) {
+		return num1 + num2;
+	}
+
+	public static float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public static float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+
+	public static float divide(float num1, float num2) {
+		return num1 / num2;
+	}
+
+	public static float square(float num1) {
+		return num1 * num1;
+	}
+
+	public static float cube(float num1) {
+		return num1 * num1 * num1;
+	}
+
+	public static float power(float num1, float num2) {
+		return (float) Math.pow(num1, num2);
+	}
+
+	public static float mod(float num1, float num2) {
+		return num1 % num2;
+	}
+}
